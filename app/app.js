7b9990f69ce9c963761f4b7bcea2259a279a9022
GAsdeX <grinya2102@gmail.com>
2017-06-12 12:17:19 +0300

    ttt

--- app/app.js
+++ app/app.js
@@ -1,17 +1,44 @@
 (function(){                                                     // Объявляю анонимную функци
   var DashbordApp = angular.module('DashbordApp', ['ngRoute']);  //
 
+  DashbordApp.factory('cardFactory',function(){
+    return {
+      card : NaN,
+      style : NaN
+    }
+  })
+
+  DashbordApp.factory('userFactory',function(){
+    return {
+      name : NaN,
+      password : NaN
+    }
+  })
+
   DashbordApp.config(function($routeProvider){
     $routeProvider
-      .when('/dashbord',{
+      .when('/',{
+        templateUrl: 'public/views/login.html',
+        controller: 'authCtrl'
+      }).when('/signup',{
+        templateUrl: 'public/views/signup.html',
+        controller: 'authCtrl'
+      }).when('/dashbord',{
         templateUrl: 'public/views/dashbord.html',
         controller: 'dashbordCtrl'
       })
+
       .otherwise({
         redirectTo : '/'
+
       });
   });
-  DashbordApp.controller('dashbordCtrl', function($scope, $http){
+
+[{"_id":"593d6138999a437e2f40b8ae","parent":"d2","style":{"sticker_color":"#000","sticker_h":"100","sticker_w":"200","sticker_left":"100","sticker_top":"100"},"title":{"text":"title","font_size":"14","font_color":"#000"},"textarea":{"text":"lets do something really awesome","font_size":"14","fon_color":"#000"}}]
+
+
+
+  DashbordApp.controller('dashbordCtrl', function($scope, $http, cardFactory, userFactory){
 
     $scope.createCard = function() {
       $http({
@@ -26,24 +53,77 @@
           });
     }
 
-    // $scope.seashdashboard = 'd1'
+    $scope.seashdashboard  = 'd1';
 
 
 
-    $scope.addCard = function(x, y) {
-      $http({
-              method: "POST",
-              url: "/changecard",
-              data: { xpos : x,
-                      ypos : y}
-          }).then(function mySucces(response) {
-              $scope.result = response.data;
-              console.log($scope.cards);
-          }, function myError(response) {
-              $scope.myWelcome = response.statusText;
-          });
+    $scope.getCard = function (id) {       //получить параметры карточки
+
     }
 
+    $scope.sendTest = function (id) {
+      // var temp = $scope.getCard(id);
+      //   cardFactory.card = id;
+      //   cardFactory.style = temp;
+
+        $http({
+                method: "GET",
+                url: "/getCard/" + id
+            }).then(function mySucces(response) {
+                $scope.getCard = response.data;
+                console.log($scope.getCard);
+            }, function myError(response) {
+                $scope.myWelcome = response.statusText;
+            });
+
+        // var temp = $scope.getCard(id);
+        cardFactory.card = $scope.getCard._id;
+        cardFactory.style = $scope.getCard.style;
+
+
+        console.log(cardFactory.style);
+        console.log($scope.data);
+
+          $http.post('/test',$scope.getCard.style).
+          then(function(data) {
+              console.log("posted successfully");
+          },function(data) {
+              console.error("error in posting");
+          })
+    }
+
+    $scope.setParam = function (id) {      //задать прарметры фабрики указаного элемента
+
+      var temp = $scope.getCard(id);
+        cardFactory.card = id;
+        cardFactory.style = temp;
+        console.log(cardFactory.style);
+    }
+
+
+    $scope.addCard = function(x, y, id) {
+
+      $scope.data = {"x":x,
+                      "y":y };
+
+      console.log($scope.data);
+
+        $http.put('/newpos/' + id ,$scope.data).
+        then(function(data) {
+            console.log("posted successfully");
+        },function(data) {
+            console.error("error in posting");
+        })
+    }    //добавить карточку
+
+
+    $scope.saveChanges = function(id){        //сохранить изменения в карточке
+      console.log($scope.xpos);
+      console.log($scope.ypos);
+
+      $scope.addCard($scope.xpos, $scope.ypos, id);
+
+    }
 
     interact('.desctop .deshboard .card')
       .draggable({
@@ -73,32 +153,18 @@
 
       function dragMoveListener (event) {
         var target = event.target,
-            // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-        // translate the element
-        // target.style.webkitTransform =
-        // target.style.transform =
-        //   'translate(' + x + 'px, ' + y + 'px)';
 
         $scope.xpos = x;
         $scope.ypos = y;
 
-        // console.log();
-        //
-        // console.log($scope.xpos);
-
         target.style["left"]=  x +'px';
         target.style["top"]= (target.scrollTop )+ y + 'px';
 
-        // console.log(target.style["left"]);
-
-        // update the posiion attributes
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
 
-        $scope.addCard(x, y)
       }
 
       // this is used later in the resizing and gesture demos
@@ -116,6 +182,50 @@
           });
       console.log($scope.cards + 'vsdvsdfv');
     // }
+
+  })
+
+  DashbordApp.controller('authCtrl', function ($scope,$http,userFactory){
+    // console.log($scope.signupdata);
+    $scope.logData = function (){
+      console.log($scope.signupdata);
+      console.log($scope.user);
+      console.log($scope.pass1);
+      console.log($scope.pass2);
+    }
+
+
+
+    $scope.verification = function(){
+
+
+      $http.post('/registrate' ,{"user":$scope.user,"pass":$scope.pass}).
+      then(function(data) {
+          console.log(data.data[0]);
+          $scope.result = data.data[0];
+      },function(data) {
+          console.error("error in posting");
+
+      })
+
+      if ($scope.result._id) {
+        console.log('s');
+        userFactory.name = $scope.result.name;
+        userFactory.password = $scope.result.password;
+        console.log(window.location.href);
+        document.location.replace("/#!/dashbord");
+        console.log(window.location.href);
+      }
+
+    }
+
+
+
+    $scope.passSimilarity = function() {
+
+    }
+
+
 
   })
 
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');         //  Подключаю express
 const mongodb =  require('mongodb');        //  Подключаю mongodb
 const MongoClient = mongodb.MongoClient();  //  Подключаю могго клиента
-const ObjectID = mongodb.ObjectID();        //  Подключаю поле идентификатора базы данных монго
-
-
+const OdjectID = require('mongodb').ObjectID;       //  Подключаю поле идентификатора базы данных монго
 const bodyParser = require('body-parser');
 
+var Authorization = require('node-authorization').Authorization;
+var compileProfile = require('node-authorization').profileCompiler;
+
+var Authorization = new Authorization('UserID', compiledProfile);
+if(!Authorization.check('blog', {Tag:'DB',ID:1000001, Action:'Add'})){
+    //Report a message, and break;
+ }else{
+    //Do the add blog;
+ }
 
 // app.use(express.static('node_modules/'));
 
@@ -15,7 +22,7 @@ var io      = require('socket.io').listen(server);
 
 server.listen(app.get('port')); // not 'app.listen'!
 
-app.use(express.static('node_modules/purecss/build'));
+app.use(express.static('node_modules/purecss/build'));  //Добавляю папки с приложением в облатсь видимости приложения
 app.use('/public', express.static('public'));
 app.use(express.static('public'));
 app.use(express.static('public/js'));
@@ -23,14 +30,19 @@ app.use(express.static('node_modules/angular'));
 app.use(express.static('node_modules/angular-route'));
 app.use(express.static('app'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', function(req,res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/getCards/:parent', function(req,res){
-  db.collection('dashboard').find({parent}).toArray(function(err, result){
+  db.collection('dashboard').find({parent : req.params.parent}).toArray(function(err, result){
     if (err) throw err;
     else res.send(result)
   })
@@ -43,15 +55,22 @@ app.get('/getCards', function(req,res){
   })
 });
 
+app.get('/getCard/:_id', function(req,res){
+  console.log(';;');
+  db.collection('dashboard').find({_id : OdjectID(req.params._id)}).toArray(function(err, result){
+    if (err) throw err;
+    else res.send(result)
+  })
+});
+
 app.post('/changecard', function(req, res){
-  // console.log(req);
-})
+  console.log(req.body);
+});
 
-
-app.get('/newcard', function(req, res){
+app.get('/newcard/:parent', function(req, res){
   // console.log(req.body);
   var card =
-      { parent:"d1",
+      { parent: req.params.parent,
           style: {
             "sticker_color":"#000",
             "sticker_h":"100",
@@ -64,7 +83,8 @@ app.get('/newcard', function(req, res){
               "font_color":"#000"},
             "textarea": {"text":"lets do something really awesome",
               "font_size":"14",
-              "fon_color":"#000"}
+              "fon_color":"#000"
+            }
           }
   db.collection('dashboard').insert(card , function (err, result) {
     if (err) throw err;
@@ -72,13 +92,80 @@ app.get('/newcard', function(req, res){
   })
 });
 
+app.post('/test', function(req, res) {
+  console.log(req.body);
+})
+
+app.put('/newpos/:_id', function(req,res) {
+
+  var item = {
+    style : {
+      sticker_top : req.body.y,
+      sticker_left : req.body.x
+    }
+  }
+
+
+  console.log(item);
+
+  db.collection('dashboard').update({_id : OdjectID(req.params._id)} ,{ $set : item}, function(err, reslut) {
+    if (err) throw err;
+    else res.send(reslut)
+  })
+});
+
+app.put('/changecard/:_id', function(req, res) {
+  console.log(req.params._id);
+
+  var item = req.body
+
+  db.collection('dashboard').update({_id : OdjectID(req.params._id)} ,{ $set : item}, function(err, reslut) {
+    if (err) throw err;
+    else res.send(reslut)
+  })
+});
+
+app.delete('/changecard/:_id', function (req,res){
+  db.userdetails.remove( { "user_id" : "testuser" } )
+})
+
+// Регистрация
+
+app.post('/registrate', function(req,res){
+  console.log(req.body);
+  db.collection('users').find({name : req.body.user, password : req.body.pass}).toArray(function (err, result){
+      if (err) throw err;
+      else res.send(result);
+  });
+});
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      
+      //Begin profiles roll-in
+      //Get user raw profiles, compile them, and save the compiled profile to the user(session) object
+      ...
+      user.authProfile = compileProfile(rawProfiles);
+      //End profiles roll-in
+
+      return done(null, user);
+    });
+  }
+));
+
+
 MongoClient.connect('mongodb://localhost:27017/local', function(err, database){
   if (err) throw err;
   else {
     db = database;
 
-    server.listen(8001);
-    app.listen(8001,function(){
+    server.listen(8003);
+    app.listen(8003,function(){
       console.log('started');
     })
   }
